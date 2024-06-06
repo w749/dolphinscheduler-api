e37854439b0801c4f020e5ee387fb807aef5b10a
wangxukun <wangxukun@haohandata.com>
2024-06-06 14:36:30 +0800

    新增补数相关的请求操作

---
+++ handle/run_handle.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+import argparse
+import json
+import os.path
+import sys
+
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
+from handle import ProjectHandle
+from common import BaseResponse
+from utils import url_join, get_logger, request_post
+from datetime import datetime
+
+
+class RunHandle(object):
+    """
+    补数
+    """
+
+    def __init__(self):
+        self._logging = get_logger(os.path.basename(__file__))
+        self._project_code = str(ProjectHandle().get_project_id())
+        self._run_process_url = url_join("/projects/" + self._project_code + "/executors/start-process-instance")
+
+    def run(self, process_code, start_time, end_time, tenant_code):
+        """
+        执行补数操作
+        Args:
+            process_code: process ID
+            start_time: 补数开始时间
+            end_time: 补数结束时间
+            tenant_code: 租户
+
+        Returns:
+
+        """
+        params = {
+            "processDefinitionCode": process_code,
+            "scheduleTime": json.dumps({
+                "complementStartDate": start_time,
+                "complementEndDate": end_time
+            }),
+            "tenantCode": tenant_code,
+            "execType": "COMPLEMENT_DATA",
+            "failureStrategy": "CONTINUE",
+            "warningType": "NONE",
+            "processInstancePriority": "MEDIUM",
+            "taskDependType": "TASK_POST",
+            "complementDependentMode": "OFF_MODE",
+            "runMode": "RUN_MODE_SERIAL",
+            "executionOrder": "DESC_ORDER"
+        }
+        obj = request_post(self._run_process_url, {}, {}, params, BaseResponse)
+        if obj.code == 0:
+            self._logging.info("Complement data success for process id {} at {} to {} success."
+                               .format(process_code, start_time, end_time))
+        else:
+            self._logging.error("Complement data failed for process id {} at {} to {}, msg is '{}'"
+                                .format(process_code, start_time, end_time, obj.msg))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Dolphin Scheduler complement data operation.")
+    parser.add_argument("-c", "--code", required=True, metavar="code", help="process code, run process -l get it")
+    parser.add_argument("-s", "--start", metavar="start_time", help="start time, default now, eg: 20240420200000")
+    parser.add_argument("-e", "--end", metavar="end_time", help="end time, default now, eg: 20240420200000")
+    parser.add_argument("-t", "--tenant", metavar="tenant", default="hdfs", help="tenant code, default hdfs")
+    args = parser.parse_args()
+
+    _datetime_parse = "%Y%m%d%H%M%S"
+    _datetime_format = "%Y-%m-%d %H:%M:%S"
+    handle = RunHandle()
+
+    start = datetime.now().strftime(_datetime_format)
+    end = datetime.now().strftime(_datetime_format)
+    tenant = args.tenant
+    if args.start:
+        try:
+            start = datetime.strptime(args.start, _datetime_parse).strftime(_datetime_format)
+        except Exception:
+            print("Parse start time {} error, eg: 20240420200000".format(args.start))
+            sys.exit(-1)
+    if args.end:
+        try:
+            end = datetime.strptime(args.end, _datetime_parse).strftime(_datetime_format)
+        except Exception:
+            print("Parse end time {} error, eg: 20240420200000".format(args.end))
+            sys.exit(-1)
+
+    handle.run(args.code, start, end, tenant)
